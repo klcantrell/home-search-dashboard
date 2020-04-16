652a0f19d8dd459783d843d4ff04dcde9cf73588
klcantrell <cantrellkalalau@gmail.com>
2020-04-15 21:19:32 -0400

    Update script to simplify ECR auth and...DEPLOY

--- backend/deploy.py
+++ backend/deploy.py
@@ -5,6 +5,9 @@ import docker
 from jinja2 import Environment, FileSystemLoader
 
 ECR_REGISTRY = '815742652454.dkr.ecr.us-east-2.amazonaws.com/home-search-dashboard'
+ECS_CLUSTER = 'home-search-dashboard'
+ECS_SERVICE = 'home-search-dashboard-service'
+APP_SPEC_FILE = 'app-spec.json'
 
 # RECEIVE COMMAND LINE ARGUMENT
 parser = argparse.ArgumentParser()
@@ -27,14 +30,17 @@ docker_tag = docker_image.tag(
     ECR_REGISTRY, tag=args.version)
 print('Docker image tagged: ', docker_tag)
 
-docker_login = docker_client.login(username='AWS', password=ecr_password,
-                                   registry=ECR_REGISTRY)
-print('Response from docker login: ', docker_login)
-
-for docker_push_line in docker_client.images.push(ECR_REGISTRY, tag=args.version, stream=True, decode=True):
+for docker_push_line in docker_client.images.push(
+        ECR_REGISTRY,
+        tag=args.version,
+        stream=True,
+        decode=True,
+        auth_config={'password': ecr_password, 'username': 'AWS'}):
     print(docker_push_line)
 
 # CREATE TASK DEF FILE
+ecs_task_def_file = f'ecs-task-def-{args.version}.json'
+
 jinja_env = Environment(
     loader=FileSystemLoader('./')
 )
@@ -42,5 +48,19 @@ jinja_env = Environment(
 task_def_template = jinja_env.get_template('ecs-task-def.json')
 ecs_task_def = task_def_template.render(version=args.version)
 
-with open(f'ecs-task-def-{args.version}.json', 'w') as f:
+with open(ecs_task_def_file, 'w') as f:
     f.write(ecs_task_def)
+
+# DEPLOY!
+ecs_deploy = subprocess.run(
+    [
+        'aws', 'ecs', 'deploy',
+        '--service', ECS_SERVICE,
+        '--task-definition', ecs_task_def_file,
+        '--codedeploy-appspec', APP_SPEC_FILE,
+        '--cluster', ECS_CLUSTER
+    ],
+    stdout=subprocess.PIPE
+).stdout.decode('utf-8')
+
+print('ECS deploy response: ', ecs_deploy)
